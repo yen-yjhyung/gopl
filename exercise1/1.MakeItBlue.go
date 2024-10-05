d6979e290808c98678c62879b77aa83c1f4d7e1c
Yeon Ji Hyung <yhyu0001@student.monash.edu>
2024-10-06 00:39:25 +1000

    exercise 1 done

---
+++ exercise1/1.MakeItBlue.go
@@ -0,0 +1,28 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+// ---------------------------------------------------------
+// EXERCISE: Make It Blue
+//
+//  1. Change `color` variable's value to "blue"
+//
+//  2. Print it
+//
+// EXPECTED OUTPUT
+//  blue
+// ---------------------------------------------------------
+
+// func main() {
+// 	color := "red"
+
+// 	color = "blue"
+
+// 	fmt.Println(color)
+// }
