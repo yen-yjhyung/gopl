cc76187f478205cfab552e607b0dc9d027172dbe
Yeon Ji Hyung <yhyu0001@student.monash.edu>
2024-10-07 21:06:00 +1100

    exercise 3 - iota & exercise 4 - print format done

---
+++ exercise3/3.IotaSeasons.go
@@ -0,0 +1,34 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+// ---------------------------------------------------------
+// EXERCISE: Iota Seasons
+//
+//  Use iota to initialize the season constants.
+//
+// HINT
+//  You can change the order of the constants.
+//
+// EXPECTED OUTPUT
+//  3 6 9 12
+// ---------------------------------------------------------
+
+// func main() {
+// 	// NOTE : You should remove all the initializers below
+// 	//        first. Then use iota to fix it.
+// 	const (
+// 		Winter = (iota + 1) * 3
+// 		Spring
+// 		Summer
+// 		Fall
+// 	)
+
+// 	fmt.Println(Winter, Spring, Summer, Fall)
+// }
